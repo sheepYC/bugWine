2418e253aa4dbf9a133ab9fdef6b90edd56f2593
yangjiayoouonv <1073515964.@qq.com>
2018-12-04 20:42:03 +0800

    yclyuandibaozha

--- src/Components/Minisite/index.js
+++ src/Components/Minisite/index.js
@@ -4,9 +4,9 @@ class Minisite extends Component{
 	render(){
 		return (
 			<div>
-				<h3 className={css.yi}>你以为会有福利吗？</h3>
-				<h2 className={css.yi}>那么会不会有福利呢？</h2>
-				<h1 className={css.yi}>哈哈，消费到一定额度</h1>
+				<h3>你以为会有福利吗？</h3>
+				<h2>那么会不会有福利呢？</h2>
+				<h1>哈哈，消费到一定额度</h1>
 				<h1>才可以领取福利哟</h1>
 			</div>
 		)
